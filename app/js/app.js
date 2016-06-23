597853d18b8d911e5dc57d82073caff84e1657f7
ioctaptceb <yulia.startsev@gmail.com>
2016-06-23 17:45:25 +0200

    [#7] Ground plane

--- app/js/app.js
+++ app/js/app.js
@@ -23,6 +23,16 @@ const init = () => {
 
   scene.add( light );
 
+  let plane = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry( 10000, 10000 ),
+    new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } )
+  );
+  plane.rotation.x = -Math.PI/2;
+  plane.position.y = -300;
+  scene.add( plane );
+  plane.receiveShadow = true;
+  scene.add( new THREE.HemisphereLight( 0x443333, 0x111122 ) );
+
   renderer = new THREE.WebGLRenderer( { antialias: true } );
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
